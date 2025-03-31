e7112d80ea948b3dcb83f5edcfd58b145de255c4
tuntun <760523867@qq.com>
2025-03-31 22:07:07 +0800

    feat: 在主布局中添加背景渐变效果

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -5,6 +5,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     <>
       <Header />
       <main>{children}</main>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(149,119,198,0.3),rgba(255,255,255,0))] dark:hidden"></div>
     </>
   )
 }
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -46,7 +46,6 @@ export default async function RootLayout({
             />
             <ThemeProvider>
               {children}
-              <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(149,119,198,0.3),rgba(255,255,255,0))] dark:hidden"></div>
               <Toaster richColors />
             </ThemeProvider>
             <TailwindIndicator />
